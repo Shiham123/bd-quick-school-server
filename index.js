97df871a6dc23c810f5b7fc952ec110bc5d94bb4
webdevratul <mahibulislamratul@gmail.com>
2024-01-25 15:54:25 +0600

    for pull data

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-require('dotenv').config();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -22,8 +22,8 @@ const client = new MongoClient(uri, {
 const run = async () => {
   try {
     // await client.connect();
-    await client.db('admin').command({ ping: 1 });
-    console.log('You successfully connected to MongoDB!');
+    await client.db("admin").command({ ping: 1 });
+    console.log("You successfully connected to MongoDB!");
   } catch (error) {
     console.log(error);
   }
@@ -31,8 +31,8 @@ const run = async () => {
 
 run();
 
-app.get('/', (request, response) => {
-  response.send('database is connected');
+app.get("/", (request, response) => {
+  response.send("database is connected");
 });
 
 app.listen(port, () => {
