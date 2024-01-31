73ac81efd5a0f8001989ff86df68a6ac54620a2e
tonoy3125 <shaifshajedt@gmail.com>
2024-01-31 18:25:17 +0600

    verify token middleware added commit

--- src/Middleware/Verifytoken.js
+++ src/Middleware/Verifytoken.js
@@ -2,17 +2,26 @@ const jwt = require("jsonwebtoken");
 const { access_jwt_token } = require("../Secret");
 
 const VerifyToken = (req, res, next) => {
-  if (!req?.headers?.authorization) {
-    return res.status(401).send({ message: "unauthorized access" });
-  }
-  const token = req?.headers?.authorization.split(" ")[1];
-  jwt.verify(token, access_jwt_token, (err, decoded) => {
-    if (err) {
-      return res.status(401).send({ message: "unauthorized access" });
+  try {
+    // console.log('inside verify token', req.headers.authorization);
+
+    if (!req.headers.authorization) {
+      return res.status(401).send({ message: 'unauthorized access' });
     }
-    req.decoded = decoded;
-    next();
-  });
+
+    const token = req.headers.authorization.split(' ')[1];
+
+    jwt.verify(token, access_jwt_token, (err, decoded) => {
+      if (err) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+
+      req.decoded = decoded;
+      next();
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 module.exports = VerifyToken;
--- src/Route/UsersRoute/UsersRoute.js
+++ src/Route/UsersRoute/UsersRoute.js
@@ -6,10 +6,11 @@ const usersGetByEmailController = require("../../Controller/UsersController/user
 const usersAdminGetByEmailController = require("../../Controller/UsersController/usersAdminGetByEmailController")
 const usersPutControllers = require("../../Controller/UsersController/usersPutControllers");
 const usersPostJwtControllers = require("../../Controller/UsersController/usersPostJwtControllers");
+const verifyToken = require("../../Middleware/Verifytoken");
 
 UsersRouter.get("/users", usersGetControllers);
 UsersRouter.get("/useremail/:email", usersGetByEmailController);
-UsersRouter.get("/user/admin/:email", usersAdminGetByEmailController)
+UsersRouter.get("/user/admin/:email", verifyToken, usersAdminGetByEmailController)
 UsersRouter.post("/users", usersPostControllers);
 UsersRouter.post("/jwt", usersPostJwtControllers);
 UsersRouter.put("/useremail/:email", usersPutControllers);
