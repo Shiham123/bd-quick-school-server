95a3905ab49eae547fd5f675d196fd2c624f992d
Shiham123 <s36651786@gmail.com>
2024-02-01 11:49:18 +0600

    try catch added

--- src/Controller/QuizController/QuizUsersGetController.js
+++ src/Controller/QuizController/QuizUsersGetController.js
@@ -1,9 +1,14 @@
 const { quizUserCollection } = require('../../DatabaseConfig/Db');
 
 const quizUsersGetController = async (req, res) => {
-  const query = req.query;
-  const result = await quizUserCollection.find(query).toArray();
-  res.status(202).send(result);
+  try {
+    const query = req.query;
+    const result = await quizUserCollection.find(query).toArray();
+    res.status(202).send(result);
+  } catch (error) {
+    console.log(error);
+    res.status(405).send({ message: 'quiz user get method' });
+  }
 };
 
 module.exports = quizUsersGetController;
--- src/Controller/QuizController/QuizUsersPostController.js
+++ src/Controller/QuizController/QuizUsersPostController.js
@@ -1,10 +1,15 @@
 const { quizUserCollection } = require('../../DatabaseConfig/Db');
 
 const quizUsersPostController = async (req, res) => {
-  const email = req.params.email;
-  const data = req.body;
-  const result = await quizUserCollection.insertOne({ email: email, ...data });
-  res.status(202).send(result);
+  try {
+    const email = req.params.email;
+    const data = req.body;
+    const result = await quizUserCollection.insertOne({ email: email, ...data });
+    res.status(202).send(result);
+  } catch (error) {
+    console.log(error);
+    res.status(403).send({ message: 'post method in quiz user' });
+  }
 };
 
 module.exports = quizUsersPostController;
