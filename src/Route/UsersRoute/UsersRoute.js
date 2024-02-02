f5556a618b9a40ac68ef707f2812fcdedb87eae6
Shiham123 <s36651786@gmail.com>
2024-02-02 10:23:22 +0600

    lasted code

--- src/Route/UsersRoute/UsersRoute.js
+++ src/Route/UsersRoute/UsersRoute.js
@@ -1,15 +1,15 @@
-const express = require("express");
+const express = require('express');
 const UsersRouter = express.Router();
-const usersPostControllers = require("../../Controller/UsersController/UserPostController");
-const usersGetControllers = require("../../Controller/UsersController/UsersGetController");
-const usersGetByEmailController = require("../../Controller/UsersController/usersGetByEmailController");
-const usersPutControllers = require("../../Controller/UsersController/usersPutControllers");
-const usersPostJwtControllers = require("../../Controller/UsersController/usersPostJwtControllers");
+const usersPostControllers = require('../../Controller/UsersController/UserPostController');
+const usersGetControllers = require('../../Controller/UsersController/UsersGetController');
+const usersGetByEmailController = require('../../Controller/UsersController/usersGetByEmailController');
+const usersPutControllers = require('../../Controller/UsersController/usersPutControllers');
+const usersPostJwtControllers = require('../../Controller/UsersController/usersPostJwtControllers');
 
-UsersRouter.get("/users", usersGetControllers);
-UsersRouter.get("/useremail/:email", usersGetByEmailController);
-UsersRouter.post("/users", usersPostControllers);
-UsersRouter.post("/jwt", usersPostJwtControllers);
-UsersRouter.put("/useremail/:email", usersPutControllers);
-//
+UsersRouter.get('/users', usersGetControllers);
+UsersRouter.get('/useremail/:email', usersGetByEmailController);
+UsersRouter.post('/users', usersPostControllers);
+UsersRouter.post('/jwt', usersPostJwtControllers);
+UsersRouter.put('/useremail/:email', usersPutControllers);
+
 module.exports = UsersRouter;
