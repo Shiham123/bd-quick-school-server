89e7613985766ede8f0bb503c1405f47409ac192
Subroto Das <rupashi.bangla.club@gmail.com>
2024-02-08 22:11:31 +0600

    Reviews Section Get and delete

--- src/Route/ReviewRoute/ReviewRoute.js
+++ src/Route/ReviewRoute/ReviewRoute.js
@@ -2,11 +2,15 @@ const express = require("express");
 const ReviewGetController = require("../../Controller/ReviewController/ReviewController");
 const ReviewPostController = require("../../Controller/ReviewController/ReviewPostController");
 const ReviewPatchController = require("../../Controller/ReviewController/ReviewPatchController");
+const adminReviewGetController = require("../../Controller/ReviewController/adminReviewGetController");
+const ReviewDeleteController = require("../../Controller/ReviewController/ReviewDeleteController");
 const ReviewRoute = express.Router();
 
 //for review get
 ReviewRoute.get("/reviewget", ReviewGetController);
+ReviewRoute.get("/admin/reviews", adminReviewGetController);
 ReviewRoute.post("/reviewpost", ReviewPostController);
 ReviewRoute.patch("/update/status/:id", ReviewPatchController);
+ReviewRoute.delete("/delete/:id", ReviewDeleteController);
 
 module.exports = ReviewRoute;
