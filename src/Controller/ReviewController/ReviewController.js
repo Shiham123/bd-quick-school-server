bd4c024b3dfd4dab085d73729f04e9e1f1593060
Shiham123 <s36651786@gmail.com>
2024-02-09 20:24:22 +0600

    change confrim to accept

--- src/Controller/ReviewController/ReviewController.js
+++ src/Controller/ReviewController/ReviewController.js
@@ -1,8 +1,8 @@
-const { reviewCollection } = require("../../DatabaseConfig/Db");
+const { reviewCollection } = require('../../DatabaseConfig/Db');
 const ReviewGetController = async (req, res) => {
   try {
     const result = await reviewCollection.find().toArray();
-    const filterdata = result.filter((item) => item.status === "confirm");
+    const filterdata = result.filter((item) => item.status === 'accepted');
     return res.status(202).send(filterdata.reverse());
   } catch (error) {
     console.log(error);
