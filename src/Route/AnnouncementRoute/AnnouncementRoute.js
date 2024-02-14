1b339768c98e9e07d4ee33c626ddd4bb5fde8c61
tonoy3125 <shaifshajedt@gmail.com>
2024-02-15 02:10:16 +0600

    announcement data get by id and using get method

--- src/Route/AnnouncementRoute/AnnouncementRoute.js
+++ src/Route/AnnouncementRoute/AnnouncementRoute.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const AnnouncementPostController = require("../../Controller/AnnouncementController/AnnouncementPostController");
 const AnnouncementGetController = require("../../Controller/AnnouncementController/AnnouncementGetController");
+const AnnouncementGetByIdController = require("../../Controller/AnnouncementController/AnnouncementGetByIdController");
 const AnnouncementRoute = express.Router();
 
 
 AnnouncementRoute.get("/admin/announcement", AnnouncementGetController)
+AnnouncementRoute.get("/admin/announcements/:id", AnnouncementGetByIdController)
 AnnouncementRoute.post("/admin/announcement", AnnouncementPostController)
+// AnnouncementRoute.delete("/admin/announcement", )
 
 
 module.exports = AnnouncementRoute;
