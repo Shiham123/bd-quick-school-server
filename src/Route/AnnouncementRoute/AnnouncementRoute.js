ced5e565a8ef69271dc6602a308d975e607496a8
tonoy3125 <shaifshajedt@gmail.com>
2024-02-15 01:29:20 +0600

    announcement route get to the database and using get method

--- src/Route/AnnouncementRoute/AnnouncementRoute.js
+++ src/Route/AnnouncementRoute/AnnouncementRoute.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const AnnouncementPostController = require("../../Controller/AnnouncementController/AnnouncementPostController");
+const AnnouncementGetController = require("../../Controller/AnnouncementController/AnnouncementGetController");
 const AnnouncementRoute = express.Router();
 
 
+AnnouncementRoute.get("/admin/announcement", AnnouncementGetController)
 AnnouncementRoute.post("/admin/announcement", AnnouncementPostController)
 
 
