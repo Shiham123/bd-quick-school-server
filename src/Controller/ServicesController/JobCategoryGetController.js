df1b20c70ec344d0327f5898445240707ed8a566
sanjidatanha09 <sanjidatanha09@gmail.com>
2024-02-21 00:05:49 +0600

    jobRoute create for job preparation section

--- src/Controller/ServicesController/JobCategoryGetController.js
+++ src/Controller/ServicesController/JobCategoryGetController.js
@@ -3,11 +3,11 @@ const { servicesCollection } = require("../../DatabaseConfig/Db");
 
 const JobCategoryGetController = async (req, res) => {
     try {
-        const query = { category : true };
+        const query = { category : "Job-Preparation" };
         const cursor = servicesCollection.find(query);
         const result = await cursor.toArray();
         res.send(result);
-        console.log('Review processed successfully');
+        console.log('job-preparation category processed successfully');
     } catch (error) {
         console.error(error);
     }
