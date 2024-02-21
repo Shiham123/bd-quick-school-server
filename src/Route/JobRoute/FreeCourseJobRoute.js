b76af53991516d5771ddd42d3f4d766f57afdbae
sanjidatanha09 <sanjidatanha09@gmail.com>
2024-02-22 00:09:01 +0600

    free course route create successfully

--- src/Route/JobRoute/FreeCourseJobRoute.js
+++ src/Route/JobRoute/FreeCourseJobRoute.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const FreeCourseJobController = require("../../Controller/ServicesController/FreeCourseJobController");
+const FreeIdCourseJobController = require("../../Controller/ServicesController/FreeIdCourseJobController");
 
 const FreeCourseJobRoute = express.Router();
 
 //Get Routes
 FreeCourseJobRoute.get("/getFreeJob", FreeCourseJobController);
+
+//Get Routes
+FreeCourseJobRoute.get("/getFreeJob/:id", FreeIdCourseJobController);
 
 
 
