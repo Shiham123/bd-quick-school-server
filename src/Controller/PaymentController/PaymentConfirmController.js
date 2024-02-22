60f761bc1f47e5cb37b4234c54d74f5c8c9595ac
tonoy3125 <shaifshajedt@gmail.com>
2024-02-22 22:18:20 +0600

    update student id

--- src/Controller/PaymentController/PaymentConfirmController.js
+++ src/Controller/PaymentController/PaymentConfirmController.js
@@ -1,5 +1,6 @@
-
 const { orderCollection, userCollection } = require("../../DatabaseConfig/Db");
+
+
 
 const PaymentConfirmController = async (req, res) => {
   const email = req.query.email;
@@ -14,19 +15,27 @@ const PaymentConfirmController = async (req, res) => {
       }
     );
     if (result.modifiedCount > 0) {
-      // Update user role to "student"
-      const userUpdateResult = await userCollection.updateOne(
-        { email: email },
-        { $set: { role: "student" } }
-      );
-
-      if (userUpdateResult.modifiedCount > 0) {
-        console.log("User role updated to student");
-        res.redirect(`http://localhost:5173/MyCourses`); // TODO: netlify link
+      // Check if the user already has a student ID
+      const user = await userCollection.findOne({ email: email });
+      if (!user.studentId) {
+        // Generate a new student ID if the user doesn't have one already
+        const studentId = generateStudentID();
+        // Update user document with the new student ID
+        const userUpdateResult = await userCollection.updateOne(
+          { email: email },
+          { $set: { role: "student", studentId: studentId } }
+        );
+        if (userUpdateResult.modifiedCount > 0) {
+          console.log("Student ID generated and user role updated to student");
+        } else {
+          console.log("Failed to generate student ID or update user role");
+          res.status(500).send("Failed to generate student ID or update user role");
+          return;
+        }
       } else {
-        console.log("Failed to update user role");
-        res.status(500).send("Failed to update user role");
+        console.log("User already has a student ID");
       }
+      res.redirect(`http://localhost:5173/MyCourses`); // TODO : Netlify Link
     } else {
       console.log("Failed to update payment status");
       res.status(500).send("Failed to update payment status");
@@ -35,7 +44,6 @@ const PaymentConfirmController = async (req, res) => {
     console.log(error);
     res.status(500).send("Internal Server Error");
   }
-  
 };
 
 module.exports = PaymentConfirmController;
