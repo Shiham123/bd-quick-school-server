e1d576ddda8780663a0580222386b70577a6443f
tonoy3125 <shaifshajedt@gmail.com>
2024-02-27 16:32:35 +0600

    notification get route added

--- src/Route/NotificationRoute/NotificationRoute.js
+++ src/Route/NotificationRoute/NotificationRoute.js
@@ -4,12 +4,14 @@ const NotificationRouter = express.Router();
 const NotificationsPatchByTitleController = require('../../Controller/NotificationsController/NotificationsPatchByTitleController');
 const NotificationsGetController = require('../../Controller/NotificationsController/NotificationsGetByTitleController');
 const NotificationsGetByCourseIdController = require('../../Controller/NotificationsController/NotificationsGetByCourseIdController');
+const NotificationsGetByEmailController = require('../../Controller/NotificationsController/NotificationsGetByEmailController');
 
 
 
 
 
 NotificationRouter.get('/notification', NotificationsGetController);
+NotificationRouter.get('/notification/update/:email', NotificationsGetByEmailController);
 NotificationRouter.get('/notifications/:courseId', NotificationsGetByCourseIdController);
 NotificationRouter.patch('/notification', NotificationsPatchByTitleController);
 
