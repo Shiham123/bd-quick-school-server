f3914faa1cbb90ec308ed75565aeb728abdaa183
tonoy3125 <shaifshajedt@gmail.com>
2024-02-29 19:52:24 +0600

    is exist device condition added

--- src/Controller/ActivityController/devicePostControllers.js
+++ src/Controller/ActivityController/devicePostControllers.js
@@ -4,9 +4,16 @@ const { activityCollection } = require("../../DatabaseConfig/Db");
 const devicePostControllers = async (req, res) => {
     try {
         const { deviceInfo } = req?.body;
-        console.log(deviceInfo)
+        // console.log(deviceInfo)
+        const isExists = await activityCollection.findOne({ email: deviceInfo.email });
+        if (isExists) {
+            return res.status(200).send({ message: 'Device already exists' });
+        }
 
-        const result = await activityCollection.insertOne(deviceInfo)
+        if (!isExists) {
+            const result = await activityCollection.insertOne(deviceInfo)
+            return res.status(200).send(result);
+        }
 
         res.send(result)
 
