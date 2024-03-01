020c5dcb89b2f888a49086c60a4df5399397435c
tonoy3125 <shaifshajedt@gmail.com>
2024-03-01 12:09:23 +0600

    device get by email and using get method

--- src/Route/ActivityRoute/ActivityRoute.js
+++ src/Route/ActivityRoute/ActivityRoute.js
@@ -5,9 +5,11 @@ const ActivityRouter = express.Router();
 
 const devicePostControllers = require('../../Controller/ActivityController/devicePostControllers');
 const deviceGetController = require('../../Controller/ActivityController/deviceGetController');
+const deviceGetByEmailController = require('../../Controller/ActivityController/deviceGetByEmailController');
 
 
 ActivityRouter.get('/device', deviceGetController)
+ActivityRouter.get('/device/update/:email', deviceGetByEmailController)
 ActivityRouter.post('/device', devicePostControllers)
 
 
