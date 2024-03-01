8e5ce3ae115c5b20b14fce6d7d17ad2372dd33d2
tonoy3125 <shaifshajedt@gmail.com>
2024-03-01 19:16:17 +0600

    device activity delete by id and using delete method

--- src/Route/ActivityRoute/ActivityRoute.js
+++ src/Route/ActivityRoute/ActivityRoute.js
@@ -6,11 +6,13 @@ const ActivityRouter = express.Router();
 const devicePostControllers = require('../../Controller/ActivityController/devicePostControllers');
 const deviceGetController = require('../../Controller/ActivityController/deviceGetController');
 const deviceGetByEmailController = require('../../Controller/ActivityController/deviceGetByEmailController');
+const deviceDeleteByIdController = require('../../Controller/ActivityController/deviceDeleteByIdController');
 
 
 ActivityRouter.get('/device', deviceGetController)
 ActivityRouter.get('/device/update/:email', deviceGetByEmailController)
 ActivityRouter.post('/device', devicePostControllers)
+ActivityRouter.delete('/device/:id', deviceDeleteByIdController)
 
 
 module.exports = ActivityRouter;
